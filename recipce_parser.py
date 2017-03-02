057fb530f5f707e3467027d31af66a4cab391b8c
Christopher Mark <cmark1017@gmail.com>
2017-03-02 16:57:15 -0600

    further refined ingredient parser

--- recipce_parser.py
+++ recipce_parser.py
@@ -2,10 +2,11 @@ __author__ = 'cmark'
 from bs4 import BeautifulSoup
 import urllib2
 import json
+import re
 
 # test urls
 recipe_url = 'http://allrecipes.com/recipe/235710/chef-johns-ricotta-meatballs/?clickId=right%20rail%200&internalSource=rr_feed_recipe&referringId=235710&referringContentType=recipe'
-rec = 'http://allrecipes.com/recipe/26921/real-hummus/'
+rec = 'http://allrecipes.com/recipe/191793/country-fried-steak/?internalSource=hub%20recipe&referringContentType=search%20results&clickId=cardslot%201'
 
 # probably a better way to do this and be able to exclude plurals
 measurements = ['cup', 'cups', 'teaspoon', 'teaspoons', 'tablespoon','tablespoons','quart','quarts','gallon', 'gallons',
@@ -133,23 +134,60 @@ def scrape_x(url, component): #either ingredient or direction
             item_lst.append(elt.string.encode('ascii')) #.encode('ascii')
     return item_lst
 
-# parses a single line of ingredient... format: quantity measurement item
+# # parses a single line of ingredient... format: quantity measurement item
+# def parse_ingredient1(ln):
+#     lst = ln.split(' ')
+#     quantity = lst[0]
+#     measurement = []
+#     if '(' in lst[1]: # case where measurement has parens (e.g. '(19 oz can of beans)')
+#         i = 1
+#         end = False
+#         while end != True: #')' not in lst[i]:
+#             if ')' in lst[i]:
+#                 end = True
+#             measurement.append(lst[i])
+#             i += 1
+#         measurement = ' '.join(measurement).translate(None,"()") #strips parens off string
+#     elif '/' in lst[1]:
+#         quantity += ' {0}'.format(lst[1])
+#         i = 2
+#     elif lst[1] in measurements:
+#         measurement = lst[1]
+#         i = 2
+#     else:
+#         measurement = ''
+#         i = 1
+#     item = ' '.join(lst[i:])
+#     return quantity, measurement, item
+
 def parse_ingredient(ln):
+    if ln.endswith("to taste"):
+
+        quantity = "to taste"
+        measurement = []
+        item  = ln[:-9]
+
+        return quantity, measurement, item
     lst = ln.split(' ')
     quantity = lst[0]
     measurement = []
-    if '(' in lst[1]: # case where measurement has parens (e.g. '(19 oz can of beans)')
-        i = 1
+    i = 1
+    if '/' in lst[i]:
+        quantity += ' {0}'.format(lst[1])
+        i = 2
+    if '(' in lst[i]: # case where measurement has parens (e.g. '(19 oz can of beans)')
+        k = i
         end = False
         while end != True: #')' not in lst[i]:
-            if ')' in lst[i]:
+            if ')' in lst[k]:
                 end = True
-            measurement.append(lst[i])
-            i += 1
+            measurement.append(lst[k])
+            k += 1
         measurement = ' '.join(measurement).translate(None,"()") #strips parens off string
-    elif lst[1] in measurements:
-        measurement = lst[1]
-        i = 2
+        i = k
+    elif lst[i] in measurements:
+        measurement = lst[i]
+        i += 1
     else:
         measurement = ''
         i = 1
@@ -190,7 +228,7 @@ def main(url):
     return json.dumps(ingredients_dict, indent=2) #json.dumps returns pretty format dict
     # return directions
 
-print main(rec)
+print main(recipe_url)
 
-# print parse_ingredient('1 (28 ounce) jar marinara sauce')
-# print parse_ingredient('1 cup chopped onion')
+# print parse_ingredient1('1 1/2 (28 ounce) jar marinara sauce')
+# print parse_ingredient1('1 cup chopped onion')
